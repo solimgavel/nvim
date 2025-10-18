50a757f958a51f503e96ad36d2f31250fa116e35
Anik <solimgavel@gmail.com>
2025-10-18 13:45:51 +0300

    add termux clip off on pc

--- lua/local/termux_clipboard.lua
+++ lua/local/termux_clipboard.lua
@@ -1,12 +1,14 @@
-vim.g.clipboard = {
-  name = 'TermuxClipboard',
-  copy = {
-    ['+'] = 'termux-clipboard-set',
-    ['*'] = 'termux-clipboard-set',
-  },
-  paste = {
-    ['+'] = 'termux-clipboard-get',
-    ['*'] = 'termux-clipboard-get',
-  },
-  cache_enabled = 0,
-}
+if os.getenv('PREFIX') == '/data/data/com.termux/files/usr' then
+  vim.g.clipboard = {
+    name = 'TermuxClipboard',
+    copy = {
+      ['+'] = 'termux-clipboard-set',
+      ['*'] = 'termux-clipboard-set',
+    },
+    paste = {
+      ['+'] = 'termux-clipboard-get',
+      ['*'] = 'termux-clipboard-get',
+    },
+    cache_enabled = 0,
+  }
+end
